762d03769e663867cd3ef930ff74f8ca51b8fb16
Logan <thatsair@gmail.com>
2024-02-07 14:11:12 +1300

    rock paper scissors first to 5 functioning, small ui updatewhich could be expanded further but only for visual quality of life

--- rpsJS.js
+++ rpsJS.js
@@ -1,4 +1,8 @@
 
+
+
+
+
 function getComputerChoice() {
     
     // randomNum = Math.random(3);
@@ -6,80 +10,121 @@ function getComputerChoice() {
     // console.log(randomNum)
     
     if (randomNum === 0) {
-        return 'Rock'
+        return 'rock'
     }
     else if (randomNum === 1) {
-        return 'Paper'
+        return 'paper'
     }
     else if (randomNum === 2) {
-        return 'Scissors'
+        return 'scissors'
     }
 }
+
+let p = 0;
+let c = 0;
+let wonCheck = 0;
+let tieCheck = 0;
+
+
+const roundResult = document.querySelector('#results');
+
+const pickRock = document.querySelector('#rock');
+pickRock.addEventListener('click', () => {
+    game('rock');
+})
+
+const pickPaper = document.querySelector('#paper');
+pickPaper.addEventListener('click', () => {
+    game('paper');
+})
+
+const pickScissors = document.querySelector('#scissors');
+pickScissors.addEventListener('click', () => {
+    game('scissors');
+})
+
 
 
 // console.log('Hello1');
+ function game(choice) {
 
-
-function game() {
     
-    // console.log('Hello2');
+    let playerSelection = choice
+    let computerSelection = getComputerChoice()
 
-    let p = 0;
-    let c = 0;
+    // When the game is finished the text content is being overwritten because the game function continues to run
 
 
-    while (p <= 5 || c <= 5) {
-
-        // console.log('Hello3');
+    if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'scissors' && computerSelection === 'rock' || playerSelection === 'paper' && computerSelection === 'scissors') {
         
-        let playerSelection = prompt('Rock, Paper or Scissors?');
-        playerSelection = playerSelection.toLowerCase();
-        
-        let computerSelection = getComputerChoice()
-        computerSelection = computerSelection.toLowerCase();
-    
-        if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'scissors' && computerSelection === 'rock' || playerSelection === 'paper' && computerSelection === 'scissors') {
-            
-            p++;
-            console.log(`Player: ${p} | Computer ${c}`);
-            
-            if (p === 5) {
-                return (`You won: ${p} to ${c}`)
-            }
-        
-            else if (c === 5) {
-                return (`You lost: ${p} to ${c}`)
-            }
-                
+        p++;
+
+        hasAnybodyWon(p, c);
+
+        if (c != 0) {
+        roundResult.textContent = (`Player: ${p} | Computer ${c}`);
         }
-
-        else if (playerSelection === 'paper' && computerSelection === 'paper' || playerSelection === 'rock' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'scissors') {
             
-            console.log(`Tie Game! The score is still Player: ${p} | Computer ${c}`);
-        }
-
-
-        else if (playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper') {
-
-            c++;
-            console.log(`Player: ${p} | Computer ${c}`);
-            
-            if (p === 5) {
-                return (`You won: ${p} to ${c}`)
-            }
-        
-            else if (c === 5) {
-                return (`You lost: ${p} to ${c}`)
-            }
-        }
-
-        else {
-            console.log(`You entered: ${playerSelection}. Please enter Rock, Paper or Scissors`)
-        }
-        
     }
-    
-    
+
+    else if (playerSelection === 'paper' && computerSelection === 'paper' || playerSelection === 'rock' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'scissors') {
+        
+        roundResult.textContent = (`Tie Game! The score is still Player: ${p} | Computer ${c}`);
+        tieCheck++;
+        /* console.log(`There have been ${tieCheck} tie games`) */
+    }
+
+    else if (playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'paper') {
+
+        c++;
+        
+        hasAnybodyWon(p, c);
+
+        if (c != 0) {
+            results.textContent = (`Player: ${p} | Computer ${c}`);
+        }
+    }
+
 }
 
-game();
+function hasAnybodyWon(p1, p2) { 
+    
+    wonCheck++;
+    /* console.log(`hasAnybodyWon function was run | ${wonCheck} times`) */
+    
+    if (p == 5) {
+        
+        roundResult.textContent = (`You won: ${p1} to ${p2}`);
+        /* console.log(`You won: ${p1} to ${p2}`); */
+        // results is not being recognized in this function
+        
+        /* console.log(`p is ${p}`)
+        console.log(`c is ${c}`) */
+        p = 0;
+        c = 0;
+        playerSelection = null;
+        computerSelection = null;
+    }
+
+    else if (c == 5) {
+        
+        roundResult.textContent = (`You lost: ${p1} to ${p2}`)
+        /* console.log(`You lost: ${p1} to ${p2}`);
+        
+        console.log(`p is ${p}`)
+        console.log(`c is ${c}`) */
+        p = 0;
+        c = 0;
+        playerSelection = null;
+        computerSelection = null;
+    }
+
+    
+};
+    
+      
+    
+
+
+
+
